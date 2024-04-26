17e0952f066c189b24f81db5317c51899c38a496
PikSowl <kmazalov11@gmai.com>
2024-04-26 22:28:49 +0200

    now integrated with Google sheets

--- code/save.php
+++ code/save.php
@@ -1,23 +1,43 @@
 <?php
-function  redirectToHome()
+
+function redirectToHome(): void
 {
     header('Location: /');
     exit();
 }
 
-if (false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description'])){
-    redirectToHome();
+$URL_APP = "https://script.google.com/home/projects/15YigspP7s3wfOCQWI2UkepjJKFOnyPGASDRZAIPqp1dYTSewPOqxHQL8-/exec";
+
+function is_filled(array $details): bool
+{
+    return !empty($details['title']) && !empty($details['email']) && !empty($details['category']) && !empty($details['description']);
 }
 
-$category = $_POST['category'];
-$title = $_POST['title'];
-$desc = $_POST['description'];
-$em= $_POST['email'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = $_POST['title'];
+    $email = $_POST['email'];
+    $category = $_POST['category'];
+    $description = $_POST['description'];
 
-$filePath = "categories/{$category}/{$title}.txt";
+    $details = [
+        'title' => $title,
+        'email' => $email,
+        'category' => $category,
+        'description' => $description,
+    ];
 
-if (false === file_put_contents($filePath, $desc)) {
-    throw new Exception("Something went wrong");
+    if (!is_filled($details)) {
+        exit('Поля формы не заполнены');
+    }
+
+    $formBody = http_build_query($details, '', '&');
+
+    $response = json_decode(file_get_contents($URL_APP, false, stream_context_create([
+        'http' => [
+            'method'  => 'POST',
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'content' => $formBody,
+        ],
+    ])), true);
 }
-chmod($filePath, 0777);
 redirectToHome();
